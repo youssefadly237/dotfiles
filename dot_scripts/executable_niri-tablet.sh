770aa8cee1285c5445aeffd06a17f6f06a1934bf
J. Adly <youssefadly237@gmail.com>
2026-02-05 03:17:54 +0200

    make tablet output mapping dynamic;

--- dot_scripts/executable_niri-tablet.sh
+++ dot_scripts/executable_niri-tablet.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-CONFIG="$HOME/.config/niri/config.kdl"
+set -euo pipefail
 
-awk '
-  /tablet {/ {flag=1}
-  flag && /map-to-output/ {
-    if ($0 ~ /eDP-1/) sub("eDP-1","HDMI-A-1")
-    else if ($0 ~ /HDMI-A-1/) sub("HDMI-A-1","eDP-1")
-  }
-  {print}
-  /}/ && flag {flag=0}
-' "$CONFIG" >"$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+CONFIG="$HOME/.config/niri/tablet.kdl"
+
+mapfile -t OUTPUTS < <(niri msg outputs | awk '/^Output/ {print $NF}' | tr -d '()')
+
+[[ ${#OUTPUTS[@]} -eq 0 ]] && exit 1
+
+CURRENT=$(awk '/map-to-output/ {print $2}' "$CONFIG" | tr -d '"')
+
+NEXT="${OUTPUTS[0]}"
+for i in "${!OUTPUTS[@]}"; do
+    [[ "${OUTPUTS[$i]}" == "$CURRENT" ]] && NEXT="${OUTPUTS[$(((i + 1) % ${#OUTPUTS[@]}))]}" && break
+done
+
+sed -i "s/map-to-output \"[^\"]*\"/map-to-output \"$NEXT\"/" "$CONFIG"
 
 niri msg action load-config-file
+echo "Tablet: $CURRENT → $NEXT"
