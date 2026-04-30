e367023977f99c003962e2b3adf0276774b7c469
J. Adly <youssefadly237@gmail.com>
2026-04-30 12:43:14 +0300

    drop the completions from the dotfiles, and replace it with gen script
    lazy load nvm because what the fuck is 800ms startup time?
    fix go path es
    add haskell for no reason (I don't know how to write haskell)

---
+++ dot_zsh/functions/executable_gen-completions.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+unalias gen-comp 2>/dev/null
+
+local -A _completions=(
+  cheat    "cheat --completion zsh"
+  typst    "typst completions zsh"
+  taskbeep "taskbeep completions zsh"
+  # oven-sh/bun/issues/10897
+  bun      "bun completions"
+)
+
+
+function gen-comp() {
+  local COMP_DIR="$HOME/.zsh/completions"
+  mkdir -p "$COMP_DIR"
+  echo "Generating completions..."
+  for cmd gen_cmd in "${(@kv)_completions}"; do
+    command -v "$cmd" >/dev/null \
+      && eval "$gen_cmd" > "$COMP_DIR/_${cmd}" \
+      && echo "  ✓ $cmd"
+  done
+  echo "Done."
+  autoload -U compinit && compinit
+}
